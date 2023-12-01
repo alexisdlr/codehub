39008f5fd73a4c689fcf455d131cbc1bb1f0cc1d
alexisdlr <alexisdlr3009@hotmail.com>
2023-12-01 17:33:11 -0600

    course dashboard

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,8 @@
+import { getChapter } from "@/actions/getChapter";
+import Banner from "@/components/Banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { VideoPlayer } from "./_components/VideoPlayer";
 
 const ChapterPage = async ({
   params,
@@ -8,10 +11,55 @@ const ChapterPage = async ({
 }) => {
   const { userId } = auth();
 
-  if(!userId) return redirect('/')
+  if (!userId) return redirect("/");
 
-  
-  return <div>chapter</div>;
+  const {
+    chapter,
+    course,
+    nextChapter,
+    purchase,
+    muxData,
+    attachments,
+    userProgress,
+  } = await getChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId,
+  });
+
+  if (!chapter || !course || !nextChapter) return redirect("/");
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner
+          variant={"success"}
+          label="You already completed this chapter."
+        />
+      )}
+      {isLocked && (
+        <Banner
+          variant={"warning"}
+          label="You need purchase this course to watch this chapter"
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={chapter.id}
+            title={chapter.title}
+            courseId={params.courseId}
+            nextChapterId={nextChapter.id}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+            playbackId={muxData?.playbackId!}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChapterPage;
